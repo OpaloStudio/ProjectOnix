2559ab21b627df46ff184e7a26572dfdd840b91c
RobertoVz <roberto@opalo.studio>
2019-02-08 16:17:41 -0600

    implementacion de sass

--- vistas/modulos/htmlCatalogo.php
+++ vistas/modulos/htmlCatalogo.php
@@ -1,6 +1,6 @@
 <div class="menuHtml">
     <div class="zonaBotonesHtml">
-        <a href="?page=5" type="button">FRAMEWORKS</a>
-        <a href="?page=6" type="button">TAGS</a>
+        <a href="?page=5" >FRAMEWORKS</a>
+        <a href="?page=6" >TAGS</a>
     </div>
 </div>
--- vistas/modulos/inicial.php
+++ vistas/modulos/inicial.php
@@ -1,9 +1,9 @@
 <div class="menuInicial">
     <div class="zonaBotonesindex">
-        <a href="?page=1" type="button">HTML</a>
-        <a href="?page=2" type="button">CSS</a>
-        <a href="?page=3"type="button">SERVER</a>
-        <a href="?page=4"type="button">JAVA SCRIPT</a>
+        <a href="?page=1" class="btnInicial" >HTML</a>
+        <a href="?page=2" class="btnInicial" >CSS</a>
+        <a href="?page=3" class="btnInicial" >SERVER</a>
+        <a href="?page=4" class="btnInicial" >JAVA SCRIPT</a>
     </div>
 </div>
 
